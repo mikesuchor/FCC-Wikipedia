7eaeb630f1aa3cec6e9a6185d5c0e76206af6d91
Michael Suchorolski <michael.suchorolski@niceincontact.com>
2018-05-11 16:17:38 -0400

    Add template strings

--- script.js
+++ script.js
@@ -7,18 +7,18 @@
 $(document).ready(function() {
     
       /* When the Search button is clicked, get and store the form input value in the searchInput variable and pass that variable to the Wikipedia API link*/
-      $(".searchButton").on("click", function() {
-        $(".results").empty();
-        var searchInput = $(".searchInput").val();
-        var wikiapi = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchInput + "&limit=10&namespace=0&format=json&callback=?";
+      $('.searchButton').on('click', function() {
+        $('.results').empty();
+        var searchInput = $('.searchInput').val();
+        var wikiapi = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchInput}&limit=10&namespace=0&format=json&callback=?`;
         
         /* Loops through the number of headers and dumps the information for the headers, description and links to the HTML */
         $.getJSON(wikiapi, function(json) {
           if (json[1].length === 0) {
-            $(".results").append("<h1>No matches found</h1>");
+            $('.results').append('<h1>No matches found</h1>');
           }
           for (var i = 0; i < json[1].length; i++) {
-              $(".results").append("<li><h1><a href=" + json[3][i] + ">" + json[1][i] + "</a></h1>" + "<p>" + json[2][i] + "</p></li>");
+              $('.results').append(`<li><h1><a href=${json[3][i]}>${json[1][i]}</a></h1><p>${json[2][i]}</p></li>`);
           }
         });
       });
